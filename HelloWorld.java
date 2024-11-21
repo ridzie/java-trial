caa7c4befec1413dde9edc82a2b5a2c9d026ed0f
Ridhima <edu.ridhima@gmail.com>
2024-11-21 20:13:18 +0100

    added another line to print

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("we will we will rock you");
+        System.out.println("we dont know if we like people");
     }
 }
